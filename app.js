e57a4138aef931ad3ba29af80c0727aea56191e0
WuWeiTsung <senby298@gmail.com>
2023-02-08 20:22:40 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
+const app = express()
 const port = 3000
+
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('MongoDB error')
+})
+
+db.once('open', () => {
+    console.log('MongoDB connection')
+})
+
 
 app.get('/', (req, res) => {
     res.send('Project init')
 })
 
 app.listen(port, () => {
-    console.log(`app is listening on port ${port}`)
+    console.log(`app is running on http://localhost:${port}`)
 })
