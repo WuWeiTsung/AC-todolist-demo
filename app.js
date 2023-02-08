d048bfb6353c58abaab2ad6f92b0dfa6a383892e
WuWeiTsung <senby298@gmail.com>
2023-02-09 02:37:42 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-
+const exphbs = require('express-handlebars')
+//port setting
 const app = express()
 const port = 3000
 
+//mongoDB settings
+require('dotenv').config()
 mongoose.set("strictQuery", false)
 mongoose.connect(process.env.MONGODB_URI)
-
 const db = mongoose.connection
-
 db.on('error', () => {
     console.log('MongoDB error')
 })
-
 db.once('open', () => {
     console.log('MongoDB connection')
 })
 
+//view engine settings(express-handlebars)
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set(`view engine`, 'hbs')
 
+//router settings
 app.get('/', (req, res) => {
-    res.send('Project init')
+    res.render('index')
 })
 
 app.listen(port, () => {
