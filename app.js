7c232cc4fbb57addbe73199d66a1e4bd47c85c45
WuWeiTsung <senby298@gmail.com>
2023-02-09 23:37:32 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const Todo = require('./models/todo')
 //port setting
@@ -25,6 +26,8 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set(`view engine`, 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
 
 //router settings
 app.get('/', (req, res) => {
@@ -62,7 +65,7 @@ app.get('/todos/:id/edit', (req, res) => {
         .catch(error => console.log(error))
 })
 
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
     const id = req.params.id
     const { name, isDone } = req.body
 
@@ -76,7 +79,7 @@ app.post('/todos/:id/edit', (req, res) => {
         .catch(errer => console.log(error))
 })
 
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
     const id = req.params.id
     return Todo.findById(id)
         .then(todo => todo.remove())
