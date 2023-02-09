c7214ac2383a49f4e3984c20c8f73f481f57e681
WuWeiTsung <senby298@gmail.com>
2023-02-09 22:59:20 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     Todo.find()
         .lean()
+        .sort({ _id: 'asc' })
         .then(todos => res.render('index', { todos }))
         .catch(error => console.error(error))
 })
@@ -68,7 +69,7 @@ app.post('/todos/:id/edit', (req, res) => {
     return Todo.findById(id)
         .then(todo => {
             todo.name = name
-            todo.isDone = isDone === "on"
+            todo.isDone = isDone === 'on'
             return todo.save()
         })
         .then(() => res.redirect(`/todos/${id}`))
