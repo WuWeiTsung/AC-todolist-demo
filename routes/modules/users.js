55ab0c501fc65f8b4056635b20ea067a68bd9334
WuWeiTsung <senby298@gmail.com>
2023-02-16 18:41:27 +0800

    fix: fix logout function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -37,7 +37,7 @@ router.post('/register', (req, res) => {
 router.get('/logout', (req, res) => {
     req.logout(() => {
         res.redirect('/users/login')
-    }).catch(error => console.log(error))
+    })
 })
 
 module.exports = router
